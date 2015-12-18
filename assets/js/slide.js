c1c59f2de5d59543a2cb66cdee5dcbdde31aaef3
Shawn <shawn524@gmail.com>
2015-12-18 16:58:36 -0500

    not sure

--- assets/js/slide.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $("[type=range]").change(function(){
-    var newval=$(this).val();
-    $("p").text(newval);
-  });
-});
-
-<input type="range" class="btn" id="bet_slide" value="0" min="25" max="200"><span>0</span>
-
